54635d944cde1af9eec2b6969e0855868ca458d9
Omer Faruk Sandikci <sandikci_omer@hotmail.com>
2022-07-20 01:35:32 +0300

    Add project files.

---
+++ TransactionMiddleware/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using TransactionMiddleware.Middlewares;
+
+namespace TransactionMiddleware.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomTransaction(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<TransactionMiddleware.Middlewares.TransactionMiddleware>();
+        }
+    }
+}
---
+++ TransactionMiddleware/Middlewares/TransactionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace TransactionMiddleware.Middlewares
+{
+    public class TransactionMiddleware
+    {
+        RequestDelegate _next;
+        public TransactionMiddleware(RequestDelegate next)
+        {
+            _next=next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            using (TransactionScope transaction=new TransactionScope())
+            {
+                try
+                {
+                    await _next(context);
+                    transaction.Complete();
+                }
+                catch (System.Exception)
+                {
+                    transaction.Dispose();
+                }
+            }
+            
+        }
+    }
+}
